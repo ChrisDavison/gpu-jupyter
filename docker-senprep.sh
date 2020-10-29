7ddeaa06a03d5f81653e11650a2483a285d2f9e5
Chris Davison <c.jr.davison@gmail.com>
2020-10-29 15:42:40 +0000

    Add some helper launcher scripts

---
+++ docker-senprep.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+    echo "No argument for port passed."
+    exit
+fi
+port=$1
+uid=$(id | sed -e 's/uid=\([0-9]\+\).*/\1/g')
+gid=$(id | sed -e 's/.*gid=\([0-9]\+\).*/\1/g')
+
+echo $uid $gid $USER
+
+docker run --gpus all -it --rm --user root -e NB_USER=$USER -e NB_UID=$uid -e NB_GID=$gid -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -p $port:8888 -v $(pwd):/home/$USER --name=gpu-jupyter-$USER gpu-jupyter-senprep
